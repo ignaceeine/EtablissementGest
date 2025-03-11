6ddd5d188fb44fa86e1fa0b794bc20a07f88389c
ignaceeine <ignaceeine@gmail.com>
2025-03-11 00:50:56 +0000

    +++++

--- resources/views/professeur/index.blade.php
+++ resources/views/professeur/index.blade.php
@@ -5,7 +5,7 @@
         <!-- Entête de la page avec titre et bouton d'ajout -->
         <div class="d-flex justify-content-between align-items-center mb-3">
             <h1>Liste des Professeurs</h1>
-            <a href="{{ route('professeur.create') }}" class="btn btn-primary">Ajouter un Professeur</a>
+            <a href="{{ route('professeur.create') }}" class="btn btn-success">Ajouter un Professeur</a>
         </div>
 
         <!-- Card contenant le tableau -->
